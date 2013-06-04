046591e1334b39dca8e9cd1061c47361b1360c44
A. Vandenbroucke <arnevdb@stanford.edu>
2013-06-04 13:54:07 -0700

    added findnext.h

--- findnext.h
+++ findnext.h
@@ -1,1 +1,4 @@
-../PSF_V4/findnext.h
+Int_t genfuncs(TF1 *cf_length[],TF1 *cf_angle[]);
+Int_t setnextstartpoint(Int_t k, Double_t xx[], Double_t yy[], TF1 *cf_length[], TF1 *cf_angle[]);
+Int_t updatepoint(TH2F *hist, Int_t k, Int_t xbin, Int_t ybin, Double_t xx[], Double_t yy[]); 
+Float_t CalcCost(TH2F *hist, Int_t k, Int_t xbin, Int_t ybin, Double_t xx[], Double_t yy[],  TF1 *cf_length[], TF1 *cf_angle[]);
