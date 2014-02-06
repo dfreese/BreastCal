549dba414f1037449e973e82e98d8b0a8dee450b
A. Vandenbroucke <arnevdb@stanford.edu>
2014-02-06 14:54:55 -0800

    updated setana_env.sh in branch ModuleClass

--- setana_env.sh
+++ setana_env.sh
@@ -1,1 +1,6 @@
-source /home/miil/MODULE_ANA/ANA_V5/ModuleClass/src/addtopath.sh /home/miil/MODULE_ANA/ANA_V5/ModuleClass
+#source /home/miil/MODULE_ANA/ANA_V5/ModuleClass/src/addtopath.sh /home/miil/MODULE_ANA/ANA_V5/ModuleClass
+# Got this from stack overflow
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $DIR
+source $DIR/src/addtopath.sh $DIR
