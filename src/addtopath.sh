7e7b2a1c9aa7ba3865434f7650230af173665552
MIIL <dfreese@stanford.edu>
2014-05-22 14:15:55 -0700

    Setting $PATH now checks if it was previously set, should allow to switch between pro and dev easily

--- src/addtopath.sh
+++ src/addtopath.sh
@@ -1,4 +1,12 @@
 
+if [ -d ${ANADIR} ]; then
+ # ANDIR PREVIOUSLY DEFINED, REMOVE FROM $LD_LIBRARY_PATH and from $PATH
+  echo " Removing previous ANADIR=${ANADIR} from path:"
+  export PATH=`echo $PATH | sed -e 's#:'"${ANADIR}"'/bin##'`
+  export LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed -e 's#:'"${ANADIR}"'/lib##'`
+  echo "PATH ::" $PATH
+fi
+
 ANADIR=$1
 
 export ANADIR
