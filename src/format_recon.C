6af6e6107fdf045935accc2fc80ff64b85e5b04c
David Freese <dfreese@stanford.edu>
2014-06-26 15:37:20 -0700

    Formatting

--- src/format_recon.C
+++ src/format_recon.C
@@ -8,32 +8,33 @@ void usage(void){
  cout << " -a : ascii  " << endl;
  cout << " -mt :: only give the first maxtime minutes " << endl;
 
-  return;}
+  return;
+}
 
 
 
 
 int main(int argc, Char_t *argv[])
 {
- 	cout << "Welcome " << endl;
+    cout << "Welcome " << endl;
 
-	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	Char_t		filename[FILENAMELENGTH] = "";
-	Int_t		verbose = 0, threshold=-1000;
-	Int_t		ix,ascii;
-	 buf buffer; 
-         Float_t PANELDISTANCE=-1; // mm
-         Bool_t RANDOMS=0;
-	 Float_t FINETIMEWINDOW=40;
-	 Int_t MAXTIME=999999999;
-	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    Char_t		filename[FILENAMELENGTH] = "";
+    Int_t		verbose = 0, threshold=-1000;
+    Int_t		ix,ascii;
+    buf buffer; 
+    Float_t PANELDISTANCE=-1; // mm
+    Bool_t RANDOMS=0;
+    Float_t FINETIMEWINDOW=40;
+    Int_t MAXTIME=999999999;
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-        ascii=0;
+    ascii=0;
 
-	for(ix = 1; ix < argc; ix++) {
+    for(ix = 1; ix < argc; ix++) {
 
-		/*
-		 * Verbose '-v'
+        /*
+         * Verbose '-v'
 		 */
 		if(strncmp(argv[ix], "-v", 2) == 0) {
 			cout << "Verbose Mode " << endl;
@@ -46,115 +47,119 @@ int main(int argc, Char_t *argv[])
 		}
 
     
-        	if(strncmp(argv[ix], "-r", 2) == 0) {
+        if(strncmp(argv[ix], "-r", 2) == 0) {
 			cout << "RANDOMS SELECTION " << endl;
 			RANDOMS = 1;
 		}
 
 
-                if (strncmp(argv[ix],"-p",2) ==0 ) {
-                  ix++;
-		  PANELDISTANCE=atof(argv[ix]);
-                  cout << " Using panel distance " << PANELDISTANCE << " mm." << endl;
-		}
- 
-		if(strncmp(argv[ix], "-a", 2) == 0) {
-			cout << "Ascii output file generated" << endl;
-			ascii = 1;
+        if (strncmp(argv[ix],"-p",2) ==0 ) {
+            ix++;
+            PANELDISTANCE=atof(argv[ix]);
+            cout << " Using panel distance " << PANELDISTANCE << " mm." << endl;
 		}
 
-
-		if(strncmp(argv[ix], "-t", 2) == 0) {
-                  FINETIMEWINDOW = atoi( argv[ix+1]);
-                  ix++;
-		}
-
-		if(strncmp(argv[ix], "-mt", 3) == 0) {
-                  MAXTIME = atoi( argv[ix+1]);
-		  cout <<"Only events within first " << MAXTIME << " minutes." << endl;
-                  ix++;
-		}
+        if(strncmp(argv[ix], "-a", 2) == 0) {
+            cout << "Ascii output file generated" << endl;
+            ascii = 1;
+        }
 
 
+        if(strncmp(argv[ix], "-t", 2) == 0) {
+            FINETIMEWINDOW = atoi( argv[ix+1]);
+            ix++;
+        }
 
-		if(strncmp(argv[ix], "-f", 2) == 0) {
+        if(strncmp(argv[ix], "-mt", 3) == 0) {
+            MAXTIME = atoi( argv[ix+1]);
+            cout <<"Only events within first " << MAXTIME << " minutes." << endl;
+            ix++;
+        }
 
-		if(strncmp(argv[ix], "-ft", 3) == 0) {
-                  threshold = atoi( argv[ix+1]);
-		  cout << "Threshold =  " << threshold << " ( not implemented yet ) " << endl;
-                  ix++;
-		}
 
-		else {
 
-		/* filename '-f' */
+        if(strncmp(argv[ix], "-f", 2) == 0) {
+            if(strncmp(argv[ix], "-ft", 3) == 0) {
+                threshold = atoi( argv[ix+1]);
+                cout << "Threshold =  " << threshold << " ( not implemented yet ) " << endl;
+                ix++;
+            }
 
-			if(strlen(argv[ix + 1]) < FILENAMELENGTH) {
-				sprintf(filename, "%s", argv[ix + 1]);
-			}
-			else {
-				cout << "Filename " << argv[ix + 1] << " too long !" << endl;
-				cout << "Exiting.." << endl;
-				return -99;
-			}
-		}
+            else {
 
+                /* filename '-f' */
+
+                if(strlen(argv[ix + 1]) < FILENAMELENGTH) {
+                    sprintf(filename, "%s", argv[ix + 1]);
                 }
-	} // loop over arguments
+                else {
+                    cout << "Filename " << argv[ix + 1] << " too long !" << endl;
+                    cout << "Exiting.." << endl;
+                    return -99;
+                }
+            }
 
-        rootlogon(verbose);
-     
-        if (PANELDISTANCE<0) { cout << "please specify paneldistance: -p [DISTANCE].\nExiting." << endl;return -2;}
+        }
+    } // loop over arguments
 
-        if (RANDOMS) cout << " Reformatting for RANDOMS " << endl;
-        else {  cout << " Fine Time window =  " << FINETIMEWINDOW << "  " << endl;}
+    rootlogon(verbose);
 
-        Char_t filebase[FILENAMELENGTH],rootfile[FILENAMELENGTH]; 
-        Char_t asciifile[FILENAMELENGTH], outfile[FILENAMELENGTH]; 
+    if (PANELDISTANCE<0) { cout << "please specify paneldistance: -p [DISTANCE].\nExiting." << endl;return -2;}
 
-        ifstream infile;
+    if (RANDOMS) cout << " Reformatting for RANDOMS " << endl;
+    else {  cout << " Fine Time window =  " << FINETIMEWINDOW << "  " << endl;}
 
-        cout << " Opening file " << filename << endl;
-        TFile *file = new TFile(filename,"OPEN");
-        TTree *m = (TTree *) file->Get("merged");
-        CoincEvent *data = new CoincEvent();
+    Char_t filebase[FILENAMELENGTH],rootfile[FILENAMELENGTH]; 
+    Char_t asciifile[FILENAMELENGTH], outfile[FILENAMELENGTH]; 
 
-        if (!m) {            m  = (TTree *) file->Get("mana"); }  
+    ifstream infile;
 
+    cout << " Opening file " << filename << endl;
+    TFile *file = new TFile(filename,"OPEN");
+    TTree *m = (TTree *) file->Get("merged");
+    CoincEvent *data = new CoincEvent();
 
-
-
-        if (!m) { cout << " Problem reading branch 'merged' or 'mana'  from file "<<  filename << endl; 
-	  cout << " Exiting. " << endl; return -99;}
-
-	m->SetBranchAddress("Event",&data);
-
-	ofstream asciiout,outputfile;
-
-	  // Open output file - matching required format for ALEX //
-	strncpy(filebase,filename,strlen(filename)-5);
-        filebase[strlen(filename)-5]='\0';
-         sprintf(rootfile,"%s",filebase);
-         strcat(rootfile,".merged.root");
-        if (ascii){
-	  sprintf(asciifile,"%s.merged.ascii",filebase);
-          asciiout.open(asciifile);
-	    }
-        cout << " Opening file " << rootfile << " for writing " << endl;
-	// OPEN YOUR OUTPUTFILE HERE ! 
-     
-        if (RANDOMS) sprintf(outfile,"%s_p%.2f_random.cuda",filebase,PANELDISTANCE);
-	else  sprintf(outfile,"%s_p%.2f_t%.0f.cuda",filebase,PANELDISTANCE, FINETIMEWINDOW);
-        outputfile.open(outfile);
-
-       Long64_t entries_m = m->GetEntries();
+    if (!m) {
+        m  = (TTree *) file->Get("mana");
+    }
 
 
-       cout << " Processing " <<  entries_m  <<  " Events " << endl;
 
-      Long64_t i;
- 
-      Double_t x1,y1,z1,x2,y2,z2;
+
+    if (!m) {
+        cout << " Problem reading branch 'merged' or 'mana'  from file "<<  filename << endl; 
+        cout << " Exiting. " << endl;
+        return(-99);
+    }
+
+    m->SetBranchAddress("Event",&data);
+
+    ofstream asciiout,outputfile;
+
+    // Open output file - matching required format for ALEX //
+    strncpy(filebase,filename,strlen(filename)-5);
+    filebase[strlen(filename)-5]='\0';
+    sprintf(rootfile,"%s",filebase);
+    strcat(rootfile,".merged.root");
+    if (ascii){
+        sprintf(asciifile,"%s.merged.ascii",filebase);
+        asciiout.open(asciifile);
+    }
+    cout << " Opening file " << rootfile << " for writing " << endl;
+    // OPEN YOUR OUTPUTFILE HERE ! 
+
+    if (RANDOMS) sprintf(outfile,"%s_p%.2f_random.cuda",filebase,PANELDISTANCE);
+    else  sprintf(outfile,"%s_p%.2f_t%.0f.cuda",filebase,PANELDISTANCE, FINETIMEWINDOW);
+    outputfile.open(outfile);
+
+    Long64_t entries_m = m->GetEntries();
+
+
+    cout << " Processing " <<  entries_m  <<  " Events " << endl;
+
+    Long64_t i;
+
+    Double_t x1,y1,z1,x2,y2,z2;
 
     buffer.cdt =0;
     buffer.ri=0;
@@ -187,140 +192,142 @@ int main(int argc, Char_t *argv[])
     Long64_t firsteventtime=0;
     Long64_t firsteventtimeset=kFALSE;
 
-      for (i=0;i<entries_m;i++){
+    for (i=0;i<entries_m;i++){
 
-	m->GetEntry(i);
+        m->GetEntry(i);
 
-	// YOUR CODE HERE -- YOU HAVE ACCESS TO THE STRUCT DATA AND ITS MEMBERS TO DO WHATEVER CONSTRAINTS //
- 
-	// The different constraint here compared to merged_ana, is that we assume that merged_ana already did a valid random selection on dtc,
+        // YOUR CODE HERE -- YOU HAVE ACCESS TO THE STRUCT DATA AND ITS MEMBERS TO DO WHATEVER CONSTRAINTS //
+
+        // The different constraint here compared to merged_ana, is that we assume that merged_ana already did a valid random selection on dtc,
         // the selection is only converted to CUDA readable format. 
         if ((RANDOMS) || ( TMath::Abs(data->dtf)<FINETIMEWINDOW)) {  
-	  if ( ( data->E1<700 ) && (data->E1> 400 ) ) {
-        	  if ( ( data->E2<700 ) && (data->E2> 400 ) ) {
-		    if ( (data->crystal1>=0 ) && (data->crystal1<64 )) {
-           		    if ( (data->crystal2>=0 ) && (data->crystal2<64 )) {
+            if ( ( data->E1<700 ) && (data->E1> 400 ) ) {
+                if ( ( data->E2<700 ) && (data->E2> 400 ) ) {
+                    if ( (data->crystal1>=0 ) && (data->crystal1<64 )) {
+                        if ( (data->crystal2>=0 ) && (data->crystal2<64 )) {
 
-			      if (!firsteventtimeset){ 
-				firsteventtime=data->ct;
-				firsteventtimeset=kTRUE;
-				cout << "firsteventtime : " << firsteventtime/(60*COARSECLOCKFREQUENCY) << " min" << endl; }
+                            if (!firsteventtimeset){ 
+                                firsteventtime=data->ct;
+                                firsteventtimeset=kTRUE;
+                                cout << "firsteventtime : " << firsteventtime/(60*COARSECLOCKFREQUENCY) << " min" << endl;
+                            }
 
-			      //  if (!((data->pos == 80600 )||(data->pos == 80000))) continue;
-			
-				      if ((data->ct - firsteventtime) > MAXTIME*60*COARSECLOCKFREQUENCY) continue; 
+                            //  if (!((data->pos == 80600 )||(data->pos == 80000))) continue;
 
-			      // if (data->pos != 80000) continue;
+                            //				      if ((data->ct - firsteventtime) > MAXTIME*60*COARSECLOCKFREQUENCY) continue; 
 
-			      
-		 	      //	if ( data->apd1 ) continue;
-			      //			      if (data->m1 != 0 ) continue;
-			      //    if (data->m2 > 7 ) continue;
-                              // FIXME ::: HARDCODED VALUES !! 
-			      if (( data->fin2 ==5 ) && ( data->m2 == 2) && (data->apd2 >0 )) continue;
-			      // if (data->fin1 > 1) continue;
-
-			      /*
-                 	      if (data->pos==80000) p1++; 
-			      if (p2>384204) continue;
-			      if (data->pos==80600) p2++; 
-			      */
+                            // if (data->pos != 80000) continue;
 
 
-			      //			      420000
+                            //	if ( data->apd1 ) continue;
+                            //			      if (data->m1 != 0 ) continue;
+                            //    if (data->m2 > 7 ) continue;
+                            // FIXME ::: HARDCODED VALUES !! 
+                            //			      if (( data->fin2 ==5 ) && ( data->m2 == 2) && (data->apd2 >0 )) continue;
+                            // if (data->fin1 > 1) continue;
+
+                            /*
+                               if (data->pos==80000) p1++; 
+                               if (p2>384204) continue;
+                               if (data->pos==80600) p2++; 
+                               */
 
 
-                              y1 = TOTALPANELDISTANCE/2;
-                              y2 = -TOTALPANELDISTANCE/2;
-                              y1 +=   data->apd1*YDISTANCEBETWEENAPDS;
-                              y2 -=   data->apd2*YDISTANCEBETWEENAPDS;
-
-			      
-			      y1 +=  (( 7-TMath::Floor(data->crystal1%8) * YCRYSTALPITCH ) + 0.5  );
-			      y2 -=  (( 7-TMath::Floor(data->crystal2%8) * YCRYSTALPITCH ) + 0.5  );                              
-			      
+                            //			      420000
 
 
-			      // NOTE:: X was pointing differently before !
-
-                              x1 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m1-8)*XMODULEPITCH;  
-                              x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m2-8)*XMODULEPITCH;  
-                              x1 +=  ( TMath::Floor(data->crystal1/8)  + 0.5  )*XCRYSTALPITCH;
-                              x2 +=  ( 7-TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
-
-			      /*
-			      x1 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-data->m1)*XMODULEPITCH;  
-			      x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-m2tmp)*XMODULEPITCH;  
+                            y1 = TOTALPANELDISTANCE/2;
+                            y2 = -TOTALPANELDISTANCE/2;
+                            y1 +=   data->apd1*YDISTANCEBETWEENAPDS;
+                            y2 -=   data->apd2*YDISTANCEBETWEENAPDS;
 
 
-			      // This "x2" is for ROTATED SETUP:: x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-data->m2)*XMODULEPITCH;  
-			      //			      x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-data->m2)*XMODULEPITCH;  
-
-
-	       		      x1 +=  ( TMath::Floor(data->crystal1/8)  + 0.5  )*XCRYSTALPITCH;
-			      //   x2 +=  ( 7-TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
-	       		
-
-			      x2 +=  ( TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
-                              */
-                              
-						      /*                              
-                              z1 = -ZPITCH/2+(4-data->fin1)*ZPITCH;
-          		      z2 = -ZPITCH/2+(4-data->fin2)*ZPITCH;
-						      */
-
-                              z1 = ZPITCH/2+(data->fin1-4)*ZPITCH;
-          		      z2 = ZPITCH/2+(data->fin2-4)*ZPITCH;
-
-
-			      // z1=0;
-			      //  z2=0;
- 
-             // this is a good data now ..
-             // do something+ write to disk
-			      buffer.x1 = x1;
-                              buffer.x2 = x2;
-                              buffer.y1 = y1;
-			      buffer.y2 = y2;
-			      buffer.z1 = z1;
-			      buffer.z2 = z2;
-
-                              lines++;
-
-			      if (ascii) asciiout << x1 << " " <<y1 << " " << z1 << " " << x2 << " " << y2 << " " << z2 << endl;
-
-			      outputfile.write( (char *) &buffer, sizeof(buffer));
-                 }
+                            y1 +=  (( 7-TMath::Floor(data->crystal1%8) * YCRYSTALPITCH ) + 0.5  );
+                            y2 -=  (( 7-TMath::Floor(data->crystal2%8) * YCRYSTALPITCH ) + 0.5  );                              
 
 
 
-			    }
-		    }
-		  }
-	  }
-      
-           
-      }
+                            // NOTE:: X was pointing differently before !
 
-      cout << " p1 : "<< p1 <<" p2 : " << p2 << endl;
-       // loop over entries
-      if (ascii) asciiout.close();
+                            x1 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m1-8)*XMODULEPITCH;  
+                            x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m2-8)*XMODULEPITCH;  
+                            x1 +=  ( TMath::Floor(data->crystal1/8)  + 0.5  )*XCRYSTALPITCH;
+                            x2 +=  ( 7-TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
 
-      cout << " wrote " << lines << " LORS. " << endl;
+                            /*
+                               x1 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-data->m1)*XMODULEPITCH;  
+                               x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-m2tmp)*XMODULEPITCH;  
+
+
+                            // This "x2" is for ROTATED SETUP:: x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-data->m2)*XMODULEPITCH;  
+                            //			      x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(7-data->m2)*XMODULEPITCH;  
+
+
+                            x1 +=  ( TMath::Floor(data->crystal1/8)  + 0.5  )*XCRYSTALPITCH;
+                            //   x2 +=  ( 7-TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
+
+
+                            x2 +=  ( TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
+                            */
+
+                            /*                              
+                                                            z1 = -ZPITCH/2+(4-data->fin1)*ZPITCH;
+                                                            z2 = -ZPITCH/2+(4-data->fin2)*ZPITCH;
+                                                            */
+
+                            z1 = ZPITCH/2+(data->fin1-4)*ZPITCH;
+                            z2 = ZPITCH/2+(data->fin2-4)*ZPITCH;
+
+
+                            // z1=0;
+                            //  z2=0;
+
+                            // this is a good data now ..
+                            // do something+ write to disk
+                            buffer.x1 = x1;
+                            buffer.x2 = x2;
+                            buffer.y1 = y1;
+                            buffer.y2 = y2;
+                            buffer.z1 = z1;
+                            buffer.z2 = z2;
+
+                            lines++;
+
+                            if (ascii) asciiout << x1 << " " <<y1 << " " << z1 << " " << x2 << " " << y2 << " " << z2 << endl;
+
+                            outputfile.write( (char *) &buffer, sizeof(buffer));
+                        }
+
+
+
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    cout << " p1 : "<< p1 <<" p2 : " << p2 << endl;
+    // loop over entries
+    if (ascii) asciiout.close();
+
+    cout << " wrote " << lines << " LORS. " << endl;
 
 
 #define COARSEDIFF 100       
 
 
 
-  
-  
-
-      //       calfile->Close();
 
 
 
- 
-      return 0;}
+    //       calfile->Close();
+
+
+
+
+    return(0);
+}
 
 
