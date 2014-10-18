1eaa40c17fefc970e6728f9260d0a28e87b3f4c8
David Freese <dfreese@stanford.edu>
2014-10-17 19:23:01 -0700

    Cleanup of merge_ana

--- include/cal_helper_functions.h
+++ include/cal_helper_functions.h
@@ -7,7 +7,6 @@
 #include "TSpectrum.h"
 #include "Syspardef.h"
 
-#define CRYSTALS_PER_APD 64
 
 Int_t drawmod_crys2(TH1F *hi[MODULES_PER_FIN][APDS_PER_MODULE][CRYSTALS_PER_APD], TCanvas *ccc, Char_t filename[MAXFILELENGTH]);
 Int_t writ(TH1D *hi[PEAKS], TCanvas *ccc, Char_t filename[MAXFILELENGTH]);
