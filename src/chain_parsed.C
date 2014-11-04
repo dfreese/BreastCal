b87921c851060430e98bbb4b5500f8ec7281bf6d
David Freese <dfreese@stanford.edu>
2014-11-04 12:09:55 -0800

    astyle formatting of chain_parsed

--- src/chain_parsed.C
+++ src/chain_parsed.C
@@ -1,20 +1,20 @@
-/* AVDB 4-25-11 
-   Conversion Program, parsing RENA data to some kind of module data, and pedestal subtraction 
+/* AVDB 4-25-11
+   Conversion Program, parsing RENA data to some kind of module data, and pedestal subtraction
    Function takes tree input arguments:
    -v  :: verbosity switch
    -f  :: inputfilename
    -o  :: outputfilename
    -p  :: pedestal file list
-   only the -f switch is necessary ! 
+   only the -f switch is necessary !
 
   AVDB 8-8-12
   We are now reading an entire 4-up board
- 
+
   AVDB 8-17-12
-  Adjusting the code for PSF. 
+  Adjusting the code for PSF.
 */
 
-//#define DEBUG 
+//#define DEBUG
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,210 +39,228 @@
 void usage(void);
 
 
-void usage(void){
- cout << " Chain parsed -f [filelist]  -o [outputfilename] " <<endl;
-  return;}
+void usage(void)
+{
+    cout << " Chain parsed -f [filelist]  -o [outputfilename] " <<endl;
+    return;
+}
 
 
-int main(int argc, char *argv[]){
-  Char_t filelist[MAXFILELENGTH];
-  Char_t outfilename[MAXFILELENGTH];
-  Char_t curfilename[MAXFILELENGTH];
-  Char_t treename[40];
-  int i;
-  Bool_t verbose=0;
-  Bool_t ofilenamespec=kFALSE;
-  for (i=0;i<argc;i++) {
+int main(int argc, char *argv[])
+{
+    Char_t filelist[MAXFILELENGTH];
+    Char_t outfilename[MAXFILELENGTH];
+    Char_t curfilename[MAXFILELENGTH];
+    Char_t treename[40];
+    int i;
+    Bool_t verbose=0;
+    Bool_t ofilenamespec=kFALSE;
+    for (i=0; i<argc; i++) {
 
-    if (strncmp(argv[i],"-v",2)==0) {
-      verbose=kTRUE;
-      
+        if (strncmp(argv[i],"-v",2)==0) {
+            verbose=kTRUE;
+
+        }
+
+        if (strncmp(argv[i],"-f",2)==0) {
+            sprintf(filelist,"%s",argv[i+1]);
+            i++;
+        }
+        if (strncmp(argv[i],"-o",2)==0) {
+            sprintf(outfilename,"%s",argv[i+1]);
+            ofilenamespec = kTRUE;
+            //      cout << " outputfile = " << outfilename << endl;
+            i++;
+        }
+
     }
 
-    if (strncmp(argv[i],"-f",2)==0) {
-      sprintf(filelist,"%s",argv[i+1]);
-      i++;
+    cout << " Chaining ROOT files; output file generated will be : " << outfilename << endl;
+
+    // making dummy histogram according to http://root.cern.ch/phpBB3/viewtopic.php?f=3&t=13607
+    TH1F* dummy = new TH1F("dummy", "dummy", 10, 0, 1);
+
+    ifstream chainfile,rootfile;
+    chainfile.open(filelist);
+
+    TChain *mdata;
+    Char_t chainname[40];
+
+
+    if (!ofilenamespec) {
+        cout << " Please specify an outputfilename " << endl;
+        usage();
+        return -99;
     }
-      if (strncmp(argv[i],"-o",2)==0) { 
-      sprintf(outfilename,"%s",argv[i+1]);
-      ofilenamespec = kTRUE;
-      //      cout << " outputfile = " << outfilename << endl;        
-      i++;
-      }
+    TFile *rfile = new TFile(outfilename,"RECREATE");
 
-   }
+    //  for (i=0;i<RENACHIPS;i++){
+    sprintf(treename,"mdata");
+    sprintf(chainname,"mdata");
+    mdata = new TChain(treename,chainname);
+    //}
 
-  cout << " Chaining ROOT files; output file generated will be : " << outfilename << endl;
+    Int_t  filesread=0;
 
-  // making dummy histogram according to http://root.cern.ch/phpBB3/viewtopic.php?f=3&t=13607
-  TH1F* dummy = new TH1F("dummy", "dummy", 10, 0, 1);
+    TH1F *ETMP[CARTRIDGES_PER_PANEL][FINS_PER_CARTRIDGE][MODULES_PER_FIN][APDS_PER_MODULE];
+    TH1F *ETMP_com[CARTRIDGES_PER_PANEL][FINS_PER_CARTRIDGE][MODULES_PER_FIN][APDS_PER_MODULE];
 
-  ifstream chainfile,rootfile;
-  chainfile.open(filelist);
-
-  TChain *mdata;
-  Char_t chainname[40];
+    TFile *decodedfile;
+    Char_t filename[50];
+    Char_t tmpstring[50];
 
 
-  if (!ofilenamespec) {
-      cout << " Please specify an outputfilename " << endl;
-      usage();
-      return -99;
-    }
-  TFile *rfile = new TFile(outfilename,"RECREATE");
+    while (chainfile >> curfilename) {   // FLAU edited, see note below
 
-  //  for (i=0;i<RENACHIPS;i++){
-  sprintf(treename,"mdata");
-  sprintf(chainname,"mdata");
-  mdata = new TChain(treename,chainname);
-  //}
+        // -------------------------------------------------------
+        // FLAU edited to fix bug, and commented the below out
+        // if use f.good() to check if end of file, sometimes reads last line twice
+        // See: http://stackoverflow.com/questions/4324441/testing-stream-good-or-stream-eof-reads-last-line-twice
+        // which says: "All of the stream state functions – fail, bad, eof, and good – tell you the
+        // current state of the stream rather than predicting the success of a future operation. Check the
+        // stream itself (which is equivalent to an inverted fail check) after the desired operation
+        // Also, it's best to avoid infinite loops and break whenever possible
 
-  Int_t  filesread=0;
-  
-  TH1F *ETMP[CARTRIDGES_PER_PANEL][FINS_PER_CARTRIDGE][MODULES_PER_FIN][APDS_PER_MODULE];
-  TH1F *ETMP_com[CARTRIDGES_PER_PANEL][FINS_PER_CARTRIDGE][MODULES_PER_FIN][APDS_PER_MODULE];
-
-  TFile *decodedfile;
-  Char_t filename[50];
-  Char_t tmpstring[50];
-
-
-  while (chainfile >> curfilename) {   // FLAU edited, see note below
-
-    // -------------------------------------------------------	  
-	// FLAU edited to fix bug, and commented the below out
-	// if use f.good() to check if end of file, sometimes reads last line twice 
-	// See: http://stackoverflow.com/questions/4324441/testing-stream-good-or-stream-eof-reads-last-line-twice
-	// which says: "All of the stream state functions – fail, bad, eof, and good – tell you the 
-	// current state of the stream rather than predicting the success of a future operation. Check the 
-	// stream itself (which is equivalent to an inverted fail check) after the desired operation
-	// Also, it's best to avoid infinite loops and break whenever possible
-      
-	//while (1){
-	  //if  (!(f.good())) break;
-	  //f >> curfilename;  // FLAU commented out
-    // -------------------------------------------------------	  
+        //while (1){
+        //if  (!(f.good())) break;
+        //f >> curfilename;  // FLAU commented out
+        // -------------------------------------------------------
 
         filesread++;
-        if (verbose) cout << " Files read :: " << filesread << ". Current file : " << curfilename << endl; 
+        if (verbose) {
+            cout << " Files read :: " << filesread << ". Current file : " << curfilename << endl;
+        }
 
-	//  cout << " ListOfKeys :: "  <<  gDirectory->GetListOfKeys()  <<endl;
+        //  cout << " ListOfKeys :: "  <<  gDirectory->GetListOfKeys()  <<endl;
 
 
         mdata->Add(curfilename);
 
-	
-       	decodedfile = new TFile(curfilename,"UPDATE");
 
-        if (!decodedfile || decodedfile->IsZombie()) {  
-         cout << "problems opening file " << filename << "\n.Exiting" << endl; 
-         return -11;}
- 
-	// FIXME:: Read circle centers from first file - should have a way to check if there are enough entries to have reliable centers. 
+        decodedfile = new TFile(curfilename,"UPDATE");
 
-        if (verbose) cout << " Reading circle centers " << endl;
-	
-	if ( filesread == 1 ) {
-	  for (Int_t c=0;c<CARTRIDGES_PER_PANEL;c++){ 
-	    for (Int_t r=0;r<FINS_PER_CARTRIDGE;r++){
-              sprintf(tmpstring,"timing/uu_c[%d][%d]",c,r);
-	      uu_c[c][r] = (TVector *) decodedfile->Get(tmpstring);
-              sprintf(tmpstring,"timing/vv_c[%d][%d]",c,r);
-	      vv_c[c][r] = (TVector *) decodedfile->Get(tmpstring);
-	    } //r
-	  } //c
-	}// filesread
+        if (!decodedfile || decodedfile->IsZombie()) {
+            cout << "problems opening file " << filename << "\n.Exiting" << endl;
+            return -11;
+        }
 
-        if (verbose) cout << " Reading histograms " << endl;
-	
-	//    decodedfile->ls();        
-	  for (Int_t c=0;c<CARTRIDGES_PER_PANEL;c++){ 
-	    for (Int_t f=0;f<FINS_PER_CARTRIDGE;f++){
-	      for (Int_t m=0;m<MODULES_PER_FIN;m++){
-		for (Int_t j=0;j<APDS_PER_MODULE;j++){
-		  sprintf(tmpstring,"C%dF%d/E[%d][%d][%d][%d]",c,f,c,f,m,j);  
-		  //		  if (verbose) cout <<  tmpstring  << endl;
-		  if (filesread==1) { 
-		    E[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring); 
-		    E[c][f][m][j]->SetDirectory(0);
-                   }
-		  else { 
-		    ETMP[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring); 
-	            E[c][f][m][j]->Add(ETMP[c][f][m][j],1);
-		    delete ETMP[c][f][m][j]; //}
-		  }
-		  sprintf(tmpstring,"C%dF%d/E_com[%d][%d][%d][%d]",c,f,c,f,m,j);
-		  //		  if (verbose) cout <<  tmpstring << endl ;
-		  if (filesread==1) { 
-		    E_com[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring); 
-		    E_com[c][f][m][j]->SetDirectory(0); }
-		  else { 
-		    ETMP_com[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring);  
-		    E_com[c][f][m][j]->Add(ETMP_com[c][f][m][j]);
-		    delete ETMP_com[c][f][m][j];
-		  }
-		} //j
-	      } // m
-	    } // f
-	 }//c
-	
-	  if (verbose) cout << " Closing file " << curfilename << endl;
+        // FIXME:: Read circle centers from first file - should have a way to check if there are enough entries to have reliable centers.
 
-	 decodedfile->Close();
+        if (verbose) {
+            cout << " Reading circle centers " << endl;
+        }
 
-    //	 }
-  } // while .. 
+        if ( filesread == 1 ) {
+            for (Int_t c=0; c<CARTRIDGES_PER_PANEL; c++) {
+                for (Int_t r=0; r<FINS_PER_CARTRIDGE; r++) {
+                    sprintf(tmpstring,"timing/uu_c[%d][%d]",c,r);
+                    uu_c[c][r] = (TVector *) decodedfile->Get(tmpstring);
+                    sprintf(tmpstring,"timing/vv_c[%d][%d]",c,r);
+                    vv_c[c][r] = (TVector *) decodedfile->Get(tmpstring);
+                } //r
+            } //c
+        }// filesread
 
-  chainfile.close();
- 
-  if (verbose) cout << " Writing to root file " << endl;
+        if (verbose) {
+            cout << " Reading histograms " << endl;
+        }
 
-  rfile->cd();
+        //    decodedfile->ls();
+        for (Int_t c=0; c<CARTRIDGES_PER_PANEL; c++) {
+            for (Int_t f=0; f<FINS_PER_CARTRIDGE; f++) {
+                for (Int_t m=0; m<MODULES_PER_FIN; m++) {
+                    for (Int_t j=0; j<APDS_PER_MODULE; j++) {
+                        sprintf(tmpstring,"C%dF%d/E[%d][%d][%d][%d]",c,f,c,f,m,j);
+                        //		  if (verbose) cout <<  tmpstring  << endl;
+                        if (filesread==1) {
+                            E[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring);
+                            E[c][f][m][j]->SetDirectory(0);
+                        } else {
+                            ETMP[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring);
+                            E[c][f][m][j]->Add(ETMP[c][f][m][j],1);
+                            delete ETMP[c][f][m][j]; //}
+                        }
+                        sprintf(tmpstring,"C%dF%d/E_com[%d][%d][%d][%d]",c,f,c,f,m,j);
+                        //		  if (verbose) cout <<  tmpstring << endl ;
+                        if (filesread==1) {
+                            E_com[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring);
+                            E_com[c][f][m][j]->SetDirectory(0);
+                        } else {
+                            ETMP_com[c][f][m][j]= (TH1F *) decodedfile->Get(tmpstring);
+                            E_com[c][f][m][j]->Add(ETMP_com[c][f][m][j]);
+                            delete ETMP_com[c][f][m][j];
+                        }
+                    } //j
+                } // m
+            } // f
+        }//c
 
- //	 for (i=0;i<RENACHIPS;i++){
+        if (verbose) {
+            cout << " Closing file " << curfilename << endl;
+        }
 
+        decodedfile->Close();
 
-	   mdata->Write();
-	   
-	   
+        //	 }
+    } // while ..
 
-	   for (Int_t c=0;c<CARTRIDGES_PER_PANEL;c++){
-	     for (Int_t f=0;f<FINS_PER_CARTRIDGE;f++) {
-	       sprintf(tmpstring,"C%dF%d",c,f);
-	       subdir[c][f] = rfile->mkdir(tmpstring);
-	       subdir[c][f]->cd();
-	       for (Int_t m=0;m<MODULES_PER_FIN;m++){
-		 for (Int_t  j=0;j<APDS_PER_MODULE;j++){
-		   E[c][f][m][j]->Write();
-		   E_com[c][f][m][j]->Write();
-		 } //j
-	       } //m
-	     } //f
-	   } // c
+    chainfile.close();
 
-	   
-	   rfile->cd();
+    if (verbose) {
+        cout << " Writing to root file " << endl;
+    }
 
-	   if (verbose) cout << " Writing U,V centers to roofile " << endl;
-	   TDirectory *timing =  rfile->mkdir("timing");
+    rfile->cd();
 
-    
-	   timing->cd();
-	   
-   // need to store uvcenters ::
-	   for (Int_t c=0;c<CARTRIDGES_PER_PANEL;c++){
-	     for (Int_t r=0;r<FINS_PER_CARTRIDGE;r++){
-		 if(verbose)	 cout << " C" << c<< " F" <<r << endl;
-	       sprintf(tmpstring,"uu_c[%d][%d]",c,r);
-	       uu_c[c][r]->Write(tmpstring);
-	       sprintf(tmpstring,"vv_c[%d][%d]",c,r);
-	       vv_c[c][r]->Write(tmpstring);
-	     } //r
-	   } //c
-	   
-
-	   
-rfile->Close();
+//	 for (i=0;i<RENACHIPS;i++){
 
 
-  return 0;}
+    mdata->Write();
+
+
+
+    for (Int_t c=0; c<CARTRIDGES_PER_PANEL; c++) {
+        for (Int_t f=0; f<FINS_PER_CARTRIDGE; f++) {
+            sprintf(tmpstring,"C%dF%d",c,f);
+            subdir[c][f] = rfile->mkdir(tmpstring);
+            subdir[c][f]->cd();
+            for (Int_t m=0; m<MODULES_PER_FIN; m++) {
+                for (Int_t  j=0; j<APDS_PER_MODULE; j++) {
+                    E[c][f][m][j]->Write();
+                    E_com[c][f][m][j]->Write();
+                } //j
+            } //m
+        } //f
+    } // c
+
+
+    rfile->cd();
+
+    if (verbose) {
+        cout << " Writing U,V centers to roofile " << endl;
+    }
+    TDirectory *timing =  rfile->mkdir("timing");
+
+
+    timing->cd();
+
+    // need to store uvcenters ::
+    for (Int_t c=0; c<CARTRIDGES_PER_PANEL; c++) {
+        for (Int_t r=0; r<FINS_PER_CARTRIDGE; r++) {
+            if(verbose) {
+                cout << " C" << c<< " F" <<r << endl;
+            }
+            sprintf(tmpstring,"uu_c[%d][%d]",c,r);
+            uu_c[c][r]->Write(tmpstring);
+            sprintf(tmpstring,"vv_c[%d][%d]",c,r);
+            vv_c[c][r]->Write(tmpstring);
+        } //r
+    } //c
+
+
+
+    rfile->Close();
+
+
+    return 0;
+}
