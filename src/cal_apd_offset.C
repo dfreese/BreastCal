900cb46d7118fffc35623103f0163936c5456848
David Freese <dfreese@stanford.edu>
2014-12-23 14:41:30 -0800

    modified cal_apd_offset to use new functions

--- src/cal_apd_offset.C
+++ src/cal_apd_offset.C
@@ -58,24 +58,21 @@ Float_t calfunc(
 
 int main(int argc, Char_t *argv[])
 { 
-    Int_t MOD1=6;
-    Int_t MOD2=1;
-    Int_t APD1=0;
-    Int_t APD2=0;
-    Int_t fin1=99;
-    Int_t fin2=99;
     Bool_t coarsetime=1; 
-    Int_t crystalmean=0;
     Int_t usegausfit=0;
-    Char_t histtitle[40];
     Int_t verbose = 0;
 
     Int_t DTF_low;
     Int_t DTF_hi;
     Int_t FINELIMIT(400);
-    Int_t DTFLIMIT(5);
 
     string filename;
+
+    float energy_gate_low(400);
+    float energy_gate_high(600);
+
+    bool read_per_crystal_correction(false);
+    bool write_per_crystal_correction(false);
 
     // A flag that is true unless a -n option is found in which case only the
     // calibration parameters and the associated plots are generated, but the
@@ -89,84 +86,40 @@ int main(int argc, Char_t *argv[])
 
 
     for(int ix = 1; ix < argc; ix++) {
-
         if(strncmp(argv[ix], "-v", 2) == 0) {
             cout << "Verbose Mode " << endl;
             verbose = 1;
         }
-
         if(strncmp(argv[ix], "-n", 2) == 0) {
             cout << "Calibrated Root File will not be created." << endl;
             write_out_root_file_flag = false;
         }
-
         if(strncmp(argv[ix], "-dp", 3) == 0) {
             cout << "Postscript Files will not be created." << endl;
             write_out_postscript_flag = false;
         }
-
-        if(strncmp(argv[ix], "-apd1", 5) == 0) {
-            APD1 = atoi( argv[ix+1]);
-            cout << "APD1 =  " << APD1 <<endl;
-            ix++;
-        }
-
-        if(strncmp(argv[ix], "-apd2", 5) == 0) {
-            APD2 = atoi( argv[ix+1]);
-            cout << "APD2 =  " << APD2 <<endl;
-            ix++;
-        }
-
-        if(strncmp(argv[ix], "-mod1", 5) == 0) {
-            MOD1 = atoi( argv[ix+1]);
-            cout << "MOD1 =  " << MOD1 <<endl;
-            ix++;
-        }
-
-        if(strncmp(argv[ix], "-mod2", 5) == 0) {
-            MOD2 = atoi( argv[ix+1]);
-            cout << "MOD2 =  " << MOD2 <<endl;
-            ix++;
-        }
-
-        if(strncmp(argv[ix], "-cm", 3) == 0) {
-            crystalmean=1;
-            cout << "Crystal calibration "  <<endl;
-        }
-
         if(strncmp(argv[ix], "-gf", 3) == 0) {
             usegausfit=1;
             cout << " Using Gauss Fit "  <<endl;
         }
-
-        /* filename '-f' */
         if(strncmp(argv[ix], "-f", 2) == 0) {
             if(strncmp(argv[ix], "-ft", 3) == 0) {
                 coarsetime=0;
                 ix++;
                 if (ix == argc) {
                     cout << " Please enter finelimit interval: -ft [finelimit]\nExiting. " << endl;
-                    return -20;
+                    return(-1);
                 } 
-                FINELIMIT=atoi(argv[ix]);
-                if (FINELIMIT<1) {
+                FINELIMIT = atoi(argv[ix]);
+                if (FINELIMIT < 1) {
                     cout << " Error. FINELIMIT = " << FINELIMIT << " too small. Please specify -ft [finelimit]. " << endl;
-                    cout << "Exiting." << endl; return -20;
+                    cout << "Exiting." << endl;
+                    return(-2);
                 }
-
                 cout << " Fine time interval = "  << FINELIMIT << endl;
-            } else if(strncmp(argv[ix], "-f1", 3) == 0) {
-                fin1=atoi ( argv[ix+1]); ix++;
-                cout << " Fin 1 :: "  <<fin1<< endl;
-            }  else if(strncmp(argv[ix], "-f2", 3) == 0) {
-                fin2=atoi ( argv[ix+1]) ; ix++;
-                cout << " Fin 2 :: " << fin2 <<endl;
-            } else if(strlen(argv[ix + 1]) < CALFILENAMELENGTH) {
-                filename = string(argv[ix + 1]);
             } else {
-                cout << "Filename " << argv[ix + 1] << " too long !" << endl;
-                cout << "Exiting.." << endl;
-                return -99;
+                /* filename '-f' */
+                filename = string(argv[ix + 1]);
             }
         }
     }
@@ -191,12 +144,10 @@ int main(int argc, Char_t *argv[])
         DTF_low = -300;
         DTF_hi = 300;
         FINELIMIT=400;
-        DTFLIMIT=200;
         cout << " Using Coarse limits: " << FINELIMIT << endl;
     } else {
         DTF_low = -50;
         DTF_hi = 50;
-        DTFLIMIT=5;
     }
 
 
@@ -206,6 +157,7 @@ int main(int argc, Char_t *argv[])
             for (int fin = 0; fin < FINS_PER_CARTRIDGE; fin++) {
                 for (int mod=0; mod<MODULES_PER_FIN; mod++) {
                     for (int apd=0; apd<APDS_PER_MODULE; apd++) {
+                        Char_t histtitle[40];
                         sprintf(histtitle,"apdoffset[%d][%d][%d][%d][%d]",panel, cartridge, fin, mod, apd);
                         if ((panel == 0) || (!coarsetime))  {
                             apdoffset[panel][cartridge][fin][mod][apd] = new TH1F(histtitle,histtitle,50,DTF_low,DTF_hi);
@@ -260,16 +212,10 @@ int main(int argc, Char_t *argv[])
     int panel = 0;
     for (Long64_t ii=0; ii < entries; ii++) {
         mm->GetEntry(ii);
-        if (evt->fin1>FINS_PER_CARTRIDGE) {
-            continue;
-        }
-        if ((evt->crystal1<65) && 
-            ((evt->apd1==0) || (evt->apd1==1)) &&
-            (evt->m1<MODULES_PER_FIN))
-        {
-            if ((evt->E1>400) && (evt->E1<600)) {
-                if (TMath::Abs(evt->dtc ) < 6) {
-                    if (TMath::Abs(evt->dtf ) < FINELIMIT ) {
+        if (BoundsCheckEvent(*evt) == 0) {
+            if (EnergyGateEvent(*evt, energy_gate_low, energy_gate_high) == 0) {
+                if (TMath::Abs(evt->dtc) < 6) {
+                    if (TMath::Abs(evt->dtf) < FINELIMIT) {
                         checkevts++;
                         apdoffset[panel][evt->cartridge1][evt->fin1][evt->m1][evt->apd1]->Fill(evt->dtf);
                     }
@@ -325,24 +271,18 @@ int main(int argc, Char_t *argv[])
     panel = 1;
     for (Long64_t ii=0; ii < entries; ii++) {
         mm->GetEntry(ii);
-        if (evt->fin1>FINS_PER_CARTRIDGE) continue;
-        if (evt->fin2>FINS_PER_CARTRIDGE) continue;
-        if ((evt->crystal1<65)&&((evt->apd1==0)||(evt->apd1==1))&&(evt->m1<MODULES_PER_FIN)) {
-            if ((evt->crystal2<65)&&((evt->apd2==0)||(evt->apd2==1))&&(evt->m2<MODULES_PER_FIN)) {
-                if  ((evt->E2>400)&&(evt->E2<600)) {
-                    if  ((evt->E1>400)&&(evt->E1<600)) {
-                        if (TMath::Abs(evt->dtc ) < 6 ) {
-                            if (TMath::Abs(evt->dtf ) < FINELIMIT ) {
-                                checkevts++;
-                                apdoffset[1][evt->cartridge2][evt->fin2][evt->m2][evt->apd2]->Fill(
-                                        evt->dtf - mean_apdoffset[0][evt->cartridge1][evt->fin1][evt->m1][evt->apd1]);
-                            }
-                        }
+        if (BoundsCheckEvent(*evt) == 0) {
+            if (EnergyGateEvent(*evt, energy_gate_low, energy_gate_high) == 0) {
+                if (TMath::Abs(evt->dtc) < 6) {
+                    if (TMath::Abs(evt->dtf) < FINELIMIT) {
+                        checkevts++;
+                        apdoffset[1][evt->cartridge2][evt->fin2][evt->m2][evt->apd2]->Fill(
+                                evt->dtf - mean_apdoffset[0][evt->cartridge1][evt->fin1][evt->m1][evt->apd1]);
                     }
                 }
             }
         }
-    } // loop over entries
+    }
 
     if (verbose){
         cout << " Done looping over entries " << endl;
@@ -383,57 +323,54 @@ int main(int argc, Char_t *argv[])
     // Now using these parameters, generate the [filename].apdoffcal.root file
     // with the fine timestamp calibrated by subtracting the mean_apdoffset for
     // each apd involved in the interaction
+    TH1F *tres = new TH1F("tres","Time Resolution After Time walk correction",100,-25,25);
+
+    CoincEvent *calevt = new CoincEvent();
+    TFile * calfile(0);
+    TTree * merged(0);
     if (write_out_root_file_flag) {
-        TH1F *tres = new TH1F("tres","Time Resolution After Time walk correction",100,-25,25);
-
         if (verbose) cout << " Opening file " << rootfile << " for writing " << endl;
-        TFile *calfile = new TFile(rootfile.c_str(),"RECREATE");
-        TTree *merged = new  TTree("merged","Merged and Calibrated LYSO-PSAPD data ");
-        CoincEvent *calevt = new CoincEvent();
-        merged->Branch("Event",&calevt);
-
-        checkevts=0;
-        if (verbose) cout << "filling new Tree :: " << endl;
-
-        for (Long64_t ii=0; ii<entries; ii++) {
-            mm->GetEntry(ii);
-            calevt=evt;
-            if (evt->fin1>FINS_PER_CARTRIDGE) continue;
-            if (evt->fin2>FINS_PER_CARTRIDGE) continue;
-            if ((evt->crystal1<65)&&((evt->apd1==0)||(evt->apd1==1))&&(evt->m1<MODULES_PER_FIN)) {
-                if ((evt->crystal2<65)&&((evt->apd2==0)||(evt->apd2==1))&&(evt->m2<MODULES_PER_FIN)) {
-
-                    calevt->dtf-= mean_apdoffset[0][evt->cartridge1][evt->fin1][evt->m1][evt->apd1] ;
-                    calevt->dtf-= mean_apdoffset[1][evt->cartridge2][evt->fin2][evt->m2][evt->apd2] ; 
-                    // Energy gate to put only the photopeaks in the fine time
-                    // stamp histogram for the time calibration calculation
-                    if (evt->E1>400&&evt->E1<600&&evt->E2>400&&evt->E2<600) {
-                        tres->Fill(calevt->dtf);
-                    }
-                }
-            }
-            checkevts++;
-            merged->Fill();
-        }
-
-
-        cout << " New Tree filled with " << checkevts << " events. " << endl;
-
-        merged->Write();
-        calfile->Close();
-
-       
-        // Fit the fine timestamp histogram
-            tres->Fit("gaus","","",-10,10);
- if (write_out_postscript_flag) {
-            // Then output the histogram with it's fit to a postscript file
-            c1->Clear();
-            tres->Draw();
-            string ps_tres_filename(filebase + ".tres.apdoffset.ps");
-            c1->Print(ps_tres_filename.c_str());
-        }
+        calfile = new TFile(rootfile.c_str(),"RECREATE");
+        merged = new  TTree("merged","Merged and Calibrated LYSO-PSAPD data ");
+        merged->Branch("Event", &calevt);
     }
 
+    checkevts=0;
+    if (verbose) cout << "filling new Tree :: " << endl;
+
+    for (Long64_t ii=0; ii<entries; ii++) {
+        mm->GetEntry(ii);
+        if (BoundsCheckEvent(*evt) == 0) {
+            calevt = evt;
+            calevt->dtf -= mean_apdoffset[0][evt->cartridge1][evt->fin1][evt->m1][evt->apd1];
+            calevt->dtf -= mean_apdoffset[1][evt->cartridge2][evt->fin2][evt->m2][evt->apd2]; 
+            // Energy gate to put only the photopeaks in the fine time
+            // stamp histogram for the time calibration calculation
+            if (EnergyGateEvent(*evt, energy_gate_low, energy_gate_high) == 0) {
+                tres->Fill(calevt->dtf);
+            }
+            if (write_out_root_file_flag) {
+                merged->Fill();
+                checkevts++;
+            }
+        }
+    }
+    cout << " New Tree filled with " << checkevts << " events. " << endl;
+
+    if (write_out_root_file_flag) {
+        merged->Write();
+        calfile->Close();
+    }
+
+    // Fit the fine timestamp histogram
+    tres->Fit("gaus","","",-10,10);
+    if (write_out_postscript_flag) {
+        // Then output the histogram with it's fit to a postscript file
+        c1->Clear();
+        tres->Draw();
+        string ps_tres_filename(filebase + ".tres.apdoffset.ps");
+        c1->Print(ps_tres_filename.c_str());
+    }
     return(0);
 }
 
