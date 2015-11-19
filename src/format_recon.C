2e1036285bf4ecedc4559927a168b2076a55f494
David Freese <dfreese@stanford.edu>
2015-11-18 21:35:14 -0800

    update format_recon to accept calibration inputs

--- src/format_recon.C
+++ src/format_recon.C
@@ -1,5 +1,6 @@
 #include "format_recon.h"
 #include <string>
+#include <cal_helper_functions.h>
 
 #define INCHTOMM 25.4
 #define XCRYSTALPITCH 1
@@ -10,23 +11,30 @@
 #define ZPITCH 0.0565*INCHTOMM //
 
 void usage(void){
-    cout << " format_recon [-r -v -a] -f [filename] -p [panelseparation] -t [finetimewindow] -ft [threshold] -mt [maxtime]]" <<endl;
+    cout << " format_recon [-r -v -a] -f [filename] -p [panelseparation] -t [finetimewindow] -mt [maxtime]]" <<endl;
     cout << " -r : needed for randoms " << endl;
     cout << " -v : verbose " << endl;
     cout << " -a : ascii  " << endl;
     cout << " -mt :: only give the first maxtime minutes " << endl;
     cout << " -eh: Set Upper Energy Window - Default is 700keV\n"
-         << " -el: Set Lower Energy Window - Default is 400keV\n";
+         << " -el: Set Lower Energy Window - Default is 400keV\n"
+         << " -dtc: Enable Course Timestamp Windowing\n"
+         << " -dtcl: Set Lower Course Timestamp Window - Default 0\n"
+         << " -dtcl: Set Lower Course Timestamp Window - Default 4\n"
+         << " -rcal (filename):  read in initial per crystal calibration\n"
+         << " -redcal (filename): read in an energy dependence file\n";
     return;
 }
 
 int main(int argc, char ** argv)
 {
+    if (argc == 1) {
+        usage();
+        return(0);
+    }
     cout << "Welcome " << endl;
 
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-    string filename("");
-    Int_t threshold(-1000);
+    string filename;
     int verbose(0);
     int ascii(0);
     Float_t PANELDISTANCE(-1); // mm
@@ -35,83 +43,136 @@ int main(int argc, char ** argv)
     Int_t MAXTIME(999999999);
     float energy_gate_high(700);
     float energy_gate_low(400);
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    int dtc_gate_low(0);
+    int dtc_gate_high(4);
+    bool use_dtc_gating_flag(false);
+    bool read_per_crystal_correction(false);
+    string input_crystal_cal_filename;
 
+    bool read_per_crystal_energy_correction(false);
+    string input_per_crystal_energy_cal_filename;
+
+
+    // Options not requiring input
     for(int ix = 1; ix < argc; ix++) {
-
-        /*
-         * Verbose '-v'
-		 */
 		if(strncmp(argv[ix], "-v", 2) == 0) {
 			cout << "Verbose Mode " << endl;
 			verbose = 1;
 		}
-
-		if(strncmp(argv[ix], "-h", 2) == 0) {
+		if(strcmp(argv[ix], "-h") == 0) {
 		  usage();
-		  return 0;
+		  return(0);
 		}
-
-        if(strncmp(argv[ix], "-r", 2) == 0) {
+        if(strcmp(argv[ix], "-r") == 0) {
 			cout << "RANDOMS SELECTION " << endl;
 			RANDOMS = 1;
 		}
-
-        if (strncmp(argv[ix],"-p",2) ==0 ) {
-            ix++;
-            PANELDISTANCE=atof(argv[ix]);
-            cout << " Using panel distance " << PANELDISTANCE << " mm." << endl;
-		}
-
-        if(strncmp(argv[ix], "-a", 2) == 0) {
+        if(strcmp(argv[ix], "-a") == 0) {
             cout << "Ascii output file generated" << endl;
             ascii = 1;
         }
-
-        if(strncmp(argv[ix], "-t", 2) == 0) {
-            FINETIMEWINDOW = atoi( argv[ix+1]);
-            ix++;
+        if(strcmp(argv[ix], "-dtc") == 0) {
+            use_dtc_gating_flag = true;
         }
+    }
 
-        if(strncmp(argv[ix], "-mt", 3) == 0) {
+    // Options requiring input
+    for(int ix = 1; ix < (argc - 1); ix++) {
+        if (strcmp(argv[ix],"-p") ==0 ) {
+            PANELDISTANCE=atof(argv[ix + 1]);
+            cout << " Using panel distance " << PANELDISTANCE << " mm." << endl;
+		}
+        if(strcmp(argv[ix], "-t") == 0) {
+            FINETIMEWINDOW = atoi(argv[ix + 1]);
+        }
+        if(strcmp(argv[ix], "-mt") == 0) {
             MAXTIME = atoi( argv[ix+1]);
             cout <<"Only events within first " << MAXTIME << " minutes." << endl;
-            ix++;
         }
-
-        if(strncmp(argv[ix], "-f", 2) == 0) {
-            if(strncmp(argv[ix], "-ft", 3) == 0) {
-                threshold = atoi( argv[ix+1]);
-                cout << "Threshold =  " << threshold << " ( not implemented yet ) " << endl;
-                ix++;
-            } else {
-                /* filename '-f' */
-                filename = string(argv[ix+1]);
-            }
-
+        if(strcmp(argv[ix], "-f") == 0) {
+            filename = string(argv[ix + 1]);
         }
-        
-        if(strncmp(argv[ix], "-eh", 3) == 0) {
+        if(strcmp(argv[ix], "-eh") == 0) {
             stringstream ss;
             ss << argv[ix + 1];
             ss >> energy_gate_high;
-            ix++;
         }
-        if(strncmp(argv[ix], "-el", 3) == 0) {
+        if(strcmp(argv[ix], "-el") == 0) {
             stringstream ss;
             ss << argv[ix + 1];
             ss >> energy_gate_low;
-            ix++;
         }
-
-    } // loop over arguments
+        if(strcmp(argv[ix], "-dtcl") == 0) {
+            stringstream ss;
+            ss << argv[ix + 1];
+            ss >> dtc_gate_low;
+            use_dtc_gating_flag = true;
+        }
+        if(strcmp(argv[ix], "-dtch") == 0) {
+            stringstream ss;
+            ss << argv[ix + 1];
+            ss >> dtc_gate_high;
+            use_dtc_gating_flag = true;
+        }
+        if(strcmp(argv[ix], "-rcal") == 0) {
+            read_per_crystal_correction = true;
+            input_crystal_cal_filename = string(argv[ix + 1]);
+        }
+        if(strcmp(argv[ix], "-redcal") == 0) {
+            read_per_crystal_energy_correction = true;
+            input_per_crystal_energy_cal_filename = string(argv[ix + 1]);
+        }
+    }
 
     rootlogon(verbose);
 
     if (PANELDISTANCE<0) {
-        usage();
+        cerr << "Panel Distance not specified or invalid" << endl;
+        cerr << "Exiting" << endl;
         return(-2);
     }
+
+    float crystal_cal[SYSTEM_PANELS]
+            [CARTRIDGES_PER_PANEL]
+            [FINS_PER_CARTRIDGE]
+            [MODULES_PER_FIN]
+            [APDS_PER_MODULE]
+            [CRYSTALS_PER_APD] = {{{{{{0}}}}}};
+
+    if (read_per_crystal_correction) {
+        int cal_read_status(ReadPerCrystalCal(
+                input_crystal_cal_filename,crystal_cal));
+        if (cal_read_status < 0) {
+            cerr << "Error in reading input calibration file: "
+                 << cal_read_status << endl;
+            cerr << "Exiting.." << endl;
+            return(-3);
+        }
+    }
+
+
+    if (verbose) {
+        cout << "Reading Input crystal energy calibration file\n";
+    }
+    float crystal_edep_cal[SYSTEM_PANELS]
+            [CARTRIDGES_PER_PANEL]
+            [FINS_PER_CARTRIDGE]
+            [MODULES_PER_FIN]
+            [APDS_PER_MODULE]
+            [CRYSTALS_PER_APD]
+            [2] = {{{{{{{0}}}}}}};
+
+    if (read_per_crystal_energy_correction) {
+        int cal_read_status(ReadPerCrystalEnergyCal(
+                input_per_crystal_energy_cal_filename, crystal_edep_cal));
+        if (cal_read_status < 0) {
+            cerr << "Error in reading input calibration file: "
+                 << cal_read_status << endl;
+            cerr << "Exiting.." << endl;
+            return(-4);
+        }
+    }
+
 
     if (RANDOMS) {
         cout << " Reformatting for RANDOMS " << endl;
@@ -198,66 +259,63 @@ int main(int argc, char ** argv)
 
         // The different constraint here compared to merged_ana, is that we assume that merged_ana already did a valid random selection on dtc,
         // the selection is only converted to CUDA readable format. 
-        if ((RANDOMS) || ( TMath::Abs(data->dtf)<FINETIMEWINDOW)) {
-            if ((data->E1 < energy_gate_high) && (data->E1 > energy_gate_low)) {
-                if ((data->E2 < energy_gate_high) && (data->E2 > energy_gate_low)) {
-                    if ((data->crystal1 >= 0) && (data->crystal1 < CRYSTALS_PER_APD)) {
-                        if ((data->crystal2 >= 0) && (data->crystal2 < CRYSTALS_PER_APD)) {
-                            if (!firsteventtimeset){ 
-                                firsteventtime=data->ct;
-                                firsteventtimeset=kTRUE;
-                                cout << "firsteventtime : " << firsteventtime/(60*COARSECLOCKFREQUENCY) << " min" << endl;
-                            }
-
-                            Double_t x1 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m1-8)*XMODULEPITCH;  
-                            Double_t x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m2-8)*XMODULEPITCH;
-
-                            x1 +=  ( TMath::Floor(data->crystal1/8)  + 0.5  )*XCRYSTALPITCH;
-                            x2 +=  ( 7-TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
-
-                            Double_t y1 = -TOTALPANELDISTANCE/2;
-                            Double_t y2 = TOTALPANELDISTANCE/2;
-                            y1 -= data->apd1*YDISTANCEBETWEENAPDS;
-                            y2 += data->apd2*YDISTANCEBETWEENAPDS;
-                            y1 -= (( 7-TMath::Floor(data->crystal1%8) * YCRYSTALPITCH ) + 0.5  );
-                            y2 += (( 7-TMath::Floor(data->crystal2%8) * YCRYSTALPITCH ) + 0.5  );                              
-
-                            Double_t z1 = (((data->cartridge1 - CARTRIDGES_PER_PANEL/2.0) * FINS_PER_CARTRIDGE) + data->fin1 + 0.5) * ZPITCH;
-                            Double_t z2 = (((data->cartridge2 - CARTRIDGES_PER_PANEL/2.0) * FINS_PER_CARTRIDGE) + data->fin2 + 0.5) * ZPITCH;
-
-                            buf buffer; 
-                            buffer.x1 = x1;
-                            buffer.x2 = x2;
-                            buffer.y1 = y1;
-                            buffer.y2 = y2;
-                            buffer.z1 = z1;
-                            buffer.z2 = z2;
-                            buffer.cdt = 0;
-                            buffer.ri = 0;
-                            buffer.si = 0;
-                            buffer.Si = 0;
-
-                            lines++;
-
-                            if (ascii) {
-                                asciiout << x1 << " " <<y1 << " " << z1 << " " << x2 << " " << y2 << " " << z2 << endl;
-                            }
-
-                            outputfile.write( (char *) &buffer, sizeof(buffer));
-                        } else {
-                            events_dropped_invalid_index++;
-                        }
-                    } else {
-                        events_dropped_invalid_index++;
+        if (BoundsCheckEvent(*data) == 0) {
+            if (EnergyGateEvent(*data, energy_gate_low, energy_gate_high) == 0) {
+                data->dtf -= GetEventOffset(*data, crystal_cal);
+                data->dtf -= GetEventOffsetEdep(*data, crystal_edep_cal);
+                if (((TMath::Abs(data->dtf)<FINETIMEWINDOW) && (!use_dtc_gating_flag))
+                        || ((data->dtc >= dtc_gate_low) && (data->dtc <= dtc_gate_high) && (use_dtc_gating_flag))
+                        || (RANDOMS))
+                {
+                    if (!firsteventtimeset){ 
+                        firsteventtime=data->ct;
+                        firsteventtimeset=kTRUE;
+                        cout << "firsteventtime : " << firsteventtime/(60*COARSECLOCKFREQUENCY) << " min" << endl;
                     }
+
+                    Double_t x1 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m1-8)*XMODULEPITCH;  
+                    Double_t x2 = (XMODULEPITCH-8*XCRYSTALPITCH)/2+(data->m2-8)*XMODULEPITCH;
+
+                    x1 +=  ( TMath::Floor(data->crystal1/8)  + 0.5  )*XCRYSTALPITCH;
+                    x2 +=  ( 7-TMath::Floor(data->crystal2/8)  + 0.5  )*XCRYSTALPITCH;
+
+                    Double_t y1 = -TOTALPANELDISTANCE/2;
+                    Double_t y2 = TOTALPANELDISTANCE/2;
+                    y1 -= data->apd1*YDISTANCEBETWEENAPDS;
+                    y2 += data->apd2*YDISTANCEBETWEENAPDS;
+                    y1 -= (( 7-TMath::Floor(data->crystal1%8) * YCRYSTALPITCH ) + 0.5  );
+                    y2 += (( 7-TMath::Floor(data->crystal2%8) * YCRYSTALPITCH ) + 0.5  );                              
+
+                    Double_t z1 = (((data->cartridge1 - CARTRIDGES_PER_PANEL/2.0) * FINS_PER_CARTRIDGE) + data->fin1 + 0.5) * ZPITCH;
+                    Double_t z2 = (((data->cartridge2 - CARTRIDGES_PER_PANEL/2.0) * FINS_PER_CARTRIDGE) + data->fin2 + 0.5) * ZPITCH;
+
+                    buf buffer; 
+                    buffer.x1 = x1;
+                    buffer.x2 = x2;
+                    buffer.y1 = y1;
+                    buffer.y2 = y2;
+                    buffer.z1 = z1;
+                    buffer.z2 = z2;
+                    buffer.cdt = 0;
+                    buffer.ri = 0;
+                    buffer.si = 0;
+                    buffer.Si = 0;
+
+                    lines++;
+
+                    if (ascii) {
+                        asciiout << x1 << " " <<y1 << " " << z1 << " " << x2 << " " << y2 << " " << z2 << endl;
+                    }
+
+                    outputfile.write( (char *) &buffer, sizeof(buffer));
                 } else {
-                    events_dropped_energy_gating++;
+                    events_dropped_fine_time_window++;
                 }
             } else {
                 events_dropped_energy_gating++;
             }
         } else {
-            events_dropped_fine_time_window++;
+            events_dropped_invalid_index++;
         }
     }
     // loop over entries
