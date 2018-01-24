fc08f3bf5b79b57fd0b68c2321f5681dd24eb415
David Freese <dfreese@stanford.edu>
2018-01-24 15:37:18 -0800

    MAINT: fix up script references

--- setana_env.sh
+++ setana_env.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-# Got this from stack overflow
+# https://stackoverflow.com/a/246128/2465202
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo $DIR
-source $DIR/src/addtopath.sh $DIR
+ANA_BIN="${ANA_DIR}/bin"
+if [ -n "${ANA_BIN}" ]; then
+    if [ -d ${ANA_BIN} ]; then
+        # remove the previously defined ANADIR/bin and it's leading ':'
+        PATH=`echo $PATH | sed -e 's#:'"${ANA_BIN}"'##g'`
+        # remove the previously defined ANADIR/bin without a leading ':'
+        # couldn't get a \? escape on the : to work for some reason.
+        PATH=`echo $PATH | sed -e 's#'"${ANA_BIN}"'##g'`
+    fi
+fi
+
+ANADIR=$DIR
+ANA_BIN="${ANA_DIR}/bin"
+
+if [ -n "$PATH" ]; then
+    PATH=$PATH:$ANA_BIN
+else
+    PATH=$ANA_BIN
+fi
+
+export ANADIR
