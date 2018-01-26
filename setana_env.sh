2e506a42f7bc18417f72cda6ded5fd1f8313c985
David Freese <dfreese@stanford.edu>
2018-01-26 15:08:03 -0800

    BUG: fix sourcing script inproper references

--- setana_env.sh
+++ setana_env.sh
@@ -3,24 +3,25 @@
 # https://stackoverflow.com/a/246128/2465202
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ANA_BIN="${ANA_DIR}/bin"
-if [ -n "${ANA_BIN}" ]; then
-    if [ -d ${ANA_BIN} ]; then
+ANABIN="${ANADIR}/bin"
+if [ -n "${ANADIR}" ]; then
+    if [ -d ${ANABIN} ]; then
         # remove the previously defined ANADIR/bin and it's leading ':'
-        PATH=`echo $PATH | sed -e 's#:'"${ANA_BIN}"'##g'`
+        PATH=`echo $PATH | sed -e 's#:'"${ANABIN}"'##g'`
         # remove the previously defined ANADIR/bin without a leading ':'
         # couldn't get a \? escape on the : to work for some reason.
-        PATH=`echo $PATH | sed -e 's#'"${ANA_BIN}"'##g'`
+        PATH=`echo $PATH | sed -e 's#'"${ANABIN}"'##g'`
     fi
 fi
 
 ANADIR=$DIR
-ANA_BIN="${ANA_DIR}/bin"
+ANABIN="${ANADIR}/bin"
 
 if [ -n "$PATH" ]; then
-    PATH=$PATH:$ANA_BIN
+    PATH=$PATH:$ANABIN
 else
-    PATH=$ANA_BIN
+    PATH=$ANABIN
 fi
 
 export ANADIR
+export PATH
